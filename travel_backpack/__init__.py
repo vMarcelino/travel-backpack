71dc1dce97b0d13bf717fcf610e8a08b06dfc9a0
vMarcelino <victor.fmarcelino@gmail.com>
2019-05-09 13:14:42 -0300

    bump to 0.12.0

--- travel_backpack/__init__.py
+++ travel_backpack/__init__.py
@@ -1,6 +1,6 @@
 """travel-backpack - Some very useful functions and classes to use in day-to-day"""
 
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 __author__ = 'Victor Marcelino <victor.fmarcelino@gmail.com>'
 __all__ = []
 
